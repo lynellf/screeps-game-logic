c789c2638d4e5cf2daa2a01d23d72e14c3c2bde1
Ezell <lynellf@gmail.com>
2021-08-04 12:07:35 -0500

    auto deploy code on build

--- rollup.config.js
+++ rollup.config.js
@@ -8,25 +8,18 @@ import screeps from 'rollup-plugin-screeps';
 require('dotenv').config();
 
 const cfg = require("./screeps.json");
-const dest = process.env.DEST;
 
 /**
  * Apply secret from .env file
  * */
 function appendToken() {
-  cfg.main.token = process.env.TOKEN;
-  cfg.pserver.token = process.env.TOKEN;
-  cfg.puser.token = process.env.TOKEN;
-  cfg.season.token = process.env.TOKEN;
+  const token = process.env.TOKEN;
+  if(cfg) {
+    cfg.token = token;
+  }
 }
 
 appendToken();
-
-if (!dest) {
-  console.log("No destination specified - code will be compiled but not uploaded");
-} else if (cfg[dest] == null) {
-  throw new Error("Invalid upload destination");
-}
 
 export default {
   input: "src/main.ts",
@@ -42,6 +35,6 @@ export default {
     resolve({ rootDir: "src" }),
     commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
-    screeps({ config: cfg, dryRun: cfg == null })
+    screeps({ config: cfg })
   ]
 }
