a6be4020ed932e3ce694cc71b8a14dd0f7a6a071
Ezell <lynellf@gmail.com>
2021-08-04 08:54:46 -0500

    prep work for auto builds

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,14 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import screeps from 'rollup-plugin-screeps';
 
-let cfg;
-// const dest = process.env.DEST;
-// if (!dest) {
-//   console.log("No destination specified - code will be compiled but not uploaded");
-// } else if ((cfg = require("./screeps.json")[dest]) == null) {
-//   throw new Error("Invalid upload destination");
-// }
+const cfg = require("./screeps.json");
+const dest = process.env.DEST;
+
+if (!dest) {
+  console.log("No destination specified - code will be compiled but not uploaded");
+} else if (cfg[dest] == null) {
+  throw new Error("Invalid upload destination");
+}
 
 export default {
   input: "src/main.ts",
