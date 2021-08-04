c857da2ce11995d53c05aa7877eb62bb12e7ba3e
Ezell <lynellf@gmail.com>
2021-08-04 11:07:08 -0500

    publish code to screeps server on build

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,22 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import screeps from 'rollup-plugin-screeps';
+require('dotenv').config();
 
 const cfg = require("./screeps.json");
 const dest = process.env.DEST;
+
+/**
+ * Apply secret from .env file
+ * */
+function appendToken() {
+  cfg.main.token = process.env.TOKEN;
+  cfg.pserver.token = process.env.TOKEN;
+  cfg.puser.token = process.env.TOKEN;
+  cfg.season.token = process.env.TOKEN;
+}
+
+appendToken();
 
 if (!dest) {
   console.log("No destination specified - code will be compiled but not uploaded");
