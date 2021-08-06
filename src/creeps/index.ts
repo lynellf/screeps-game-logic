34cede82779e0f158f94c04b9ae8476bd73c3cef
Ezell <lynellf@gmail.com>
2021-08-05 22:16:25 -0500

    add aux role

---
+++ src/creeps/index.ts
@@ -0,0 +1,4 @@
+export { default as Builder } from './builder'
+export { default as Harvester } from './harvester'
+export { default as Upgrader } from './upgrader'
+export { aux as Aux } from './auxiliary'
